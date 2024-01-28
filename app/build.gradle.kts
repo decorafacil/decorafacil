99a2388c0692d4aa9246f0b13f6250df6be4d61f
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2024-01-28 02:00:31 -0300

    build: altera minSdk para 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "br.com.decorafacil"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
