6274e840ac2771ffeb436055658027e9515d6d73
Thyago Lobato <thyagollobato@gmail.com>
2024-01-31 18:14:09 -0300

    refact: Cria efeito de clique para o botão de Salvar usuário

--- app/src/main/java/br/com/decorafacil/ui/activity/UserRegisterActivity.kt
+++ app/src/main/java/br/com/decorafacil/ui/activity/UserRegisterActivity.kt
@@ -3,7 +3,9 @@ package br.com.decorafacil.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import br.com.decorafacil.R
 
 class UserRegisterActivity : AppCompatActivity() {
@@ -11,12 +13,27 @@ class UserRegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_register)
 
+        backToLoginActivity()
+        backToHomeActivityAfterSaveUserInformations()
+    }
+
+    private fun backToLoginActivity() {
         val btnBackArrow = findViewById<ImageView>(R.id.leftarrow)
 
         btnBackArrow.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+    }
 
+    private fun backToHomeActivityAfterSaveUserInformations() {
+        val btnSaveUserInformation = findViewById<ImageButton>(R.id.imageButtonSaveUser)
+
+        btnSaveUserInformation.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
+            Toast.makeText(applicationContext, "Usuário salvo com sucesso!", Toast.LENGTH_SHORT).show()
         }
     }
 }
