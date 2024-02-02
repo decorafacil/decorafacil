ae2009917bf3786a2dbdde64163f2a8fdecff31d
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2024-02-01 22:08:13 -0300

    feat: adiciona tela da segunda step do formulário de cadastro de evento

--- app/src/main/java/br/com/decorafacil/ui/activity/form/NewEventStepOneActivity.kt
+++ app/src/main/java/br/com/decorafacil/ui/activity/form/NewEventStepOneActivity.kt
@@ -1,5 +1,6 @@
 package br.com.decorafacil.ui.activity.form
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.decorafacil.databinding.ActivityNewEventStepOneBinding
@@ -12,5 +13,13 @@ class NewEventStepOneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        configComponents()
     }
+
+    private fun configComponents() {
+        binding.buttonNext.setOnClickListener {
+            startActivity(Intent(this, NewEventStepTwoActivity::class.java))
+        }
+    }
+
 }
---
+++ app/src/main/java/br/com/decorafacil/ui/activity/form/NewEventStepTwoActivity.kt
@@ -0,0 +1,92 @@
+package br.com.decorafacil.ui.activity.form
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.decorafacil.databinding.ActivityNewEventStepTwoBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class NewEventStepTwoActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityNewEventStepTwoBinding.inflate(layoutInflater)
+    }
+    private val timeFormat = TimeFormat.CLOCK_24H
+    private val datePickerEventDate =
+        MaterialDatePicker.Builder.datePicker().setTitleText("Selecione a data").build()
+    private val timePickerEventStartTime =
+        MaterialTimePicker.Builder().setTimeFormat(timeFormat).setHour(12).setMinute(0)
+            .setTitleText("Select Appointment time").build()
+    private val timePickerEventEndTime =
+        MaterialTimePicker.Builder().setTimeFormat(timeFormat).setHour(12).setMinute(0)
+            .setTitleText("Select Appointment time").build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configComponents()
+    }
+
+    private fun configComponents() {
+        configEditTextEventDate()
+        configEditTextEventStartTime()
+        configEditTextEventEndTime()
+    }
+
+    private fun configEditTextEventDate() {
+        val editTextDate = binding.editTextEventDate
+        editTextDate.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                editTextDate.clearFocus()
+                datePickerEventDate.show(supportFragmentManager, datePickerEventDate.toString())
+            }
+        }
+        datePickerEventDate.addOnPositiveButtonClickListener { selection ->
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
+            val formattedDate = dateFormat.format(Date(selection))
+            editTextDate.setText(formattedDate)
+        }
+    }
+
+    private fun configEditTextEventStartTime() {
+        val editTextStartTime = binding.editTextEventStartTime
+        editTextStartTime.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                editTextStartTime.clearFocus()
+                timePickerEventStartTime.show(
+                    supportFragmentManager,
+                    timePickerEventEndTime.toString()
+                )
+            }
+        }
+        timePickerEventStartTime.addOnPositiveButtonClickListener {
+            val minuteFormatted = "%02d".format(timePickerEventStartTime.minute)
+            val hourFormatted = "%02d".format(timePickerEventStartTime.hour)
+            editTextStartTime.setText("$hourFormatted:${minuteFormatted}")
+        }
+    }
+
+    private fun configEditTextEventEndTime() {
+        val editTextEndTime = binding.editTextEventEndTime
+        editTextEndTime.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                editTextEndTime.clearFocus()
+                timePickerEventEndTime.show(
+                    supportFragmentManager,
+                    timePickerEventEndTime.toString()
+                )
+            }
+        }
+        timePickerEventEndTime.addOnPositiveButtonClickListener {
+            val minuteFormatted = "%02d".format(timePickerEventEndTime.minute)
+            val hourFormatted = "%02d".format(timePickerEventEndTime.hour)
+            editTextEndTime.setText("$hourFormatted:$minuteFormatted")
+        }
+
+    }
+
+}
