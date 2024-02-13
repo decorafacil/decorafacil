577c8cba758336829310347c8fb56c4864013fb2
Thyago Lobato <thyagollobato@gmail.com>
2024-02-13 11:55:58 -0300

    feat: adiciona lógica para retorno no botão de voltar da tela de redefinição de senha

--- app/src/main/java/br/com/decorafacil/ui/activity/PasswordResetActivity.kt
+++ app/src/main/java/br/com/decorafacil/ui/activity/PasswordResetActivity.kt
@@ -1,12 +1,25 @@
 package br.com.decorafacil.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import br.com.decorafacil.R
 
 class PasswordResetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_reset)
+
+        backToForgotPasswordActivity()
+    }
+
+    private fun backToForgotPasswordActivity() {
+        val btnBackArrow = findViewById<ImageView>(R.id.imageViewLeftArrowPasswordReset)
+
+        btnBackArrow.setOnClickListener {
+            val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
