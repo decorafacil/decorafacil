cc1663b53bef0e06a70ccb3881572e33cad3f9e9
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2024-02-12 22:17:36 -0300

    refact: adiciona layout personalizado para AlertDialog de evento

--- app/src/main/java/br/com/decorafacil/ui/dialog/EditEventDialog.kt
+++ app/src/main/java/br/com/decorafacil/ui/dialog/EditEventDialog.kt
@@ -1,32 +1,60 @@
 package br.com.decorafacil.ui.dialog
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.res.ResourcesCompat
+import androidx.appcompat.app.AlertDialog
 import br.com.decorafacil.R
 import br.com.decorafacil.models.Event
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class EditEventDialog(private val context: Context) {
+
+    private val inflater = LayoutInflater.from(context)
+    private val view = inflater.inflate(R.layout.event_custom_dialog, null)
+    private val textViewClientName = view.findViewById<TextView>(R.id.textViewClientName)
+    private val textViewEventAddress = view.findViewById<TextView>(R.id.textViewEventAddress)
+    private val textViewBirthdayPersonName =
+        view.findViewById<TextView>(R.id.textViewBirthdayPersonName)
+    private val buttonEdit = view.findViewById<Button>(R.id.buttonEdit)
+    private val buttonCancelEvent = view.findViewById<Button>(R.id.buttonCancelEvent)
+    private val buttonBack = view.findViewById<Button>(R.id.buttonBack)
+    private val alertDialog = AlertDialog.Builder(context)
+        .setCancelable(true)
+        .create()
+
     fun show(event: Event) {
-        val resources = context.resources
-        val drawable =
-            ResourcesCompat.getDrawable(resources, R.drawable.dialog_background, null)
-        MaterialAlertDialogBuilder(
-            context,
-            R.style.ThemeOverlay_App_MaterialAlertDialog
-        )
-            .setTitle(event.client.contractor)
-            .setMessage(event.toString())
-            .setBackground(drawable)
-            .setNeutralButton("Editar") { _, _ ->
-                Toast.makeText(
-                    context,
-                    "Em desenvolvimento",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            .setNegativeButton("Fechar", null)
-            .show()
+        configComponents(event)
+        alertDialog.setView(view)
+        alertDialog.show()
+    }
+
+    private fun configComponents(event: Event) {
+        textViewClientName.text = event.client.contractor
+        textViewEventAddress.text = event.address.toString()
+        textViewBirthdayPersonName.text = "Aniversariante: ${event.client.birthdayPersonName}"
+
+        buttonEdit.setOnClickListener {
+            Toast.makeText(
+                context,
+                "Em desenvolvimento",
+                Toast.LENGTH_SHORT
+            ).show()
+            alertDialog.dismiss()
+        }
+
+        buttonCancelEvent.setOnClickListener {
+            Toast.makeText(
+                context,
+                "Em desenvolvimento",
+                Toast.LENGTH_SHORT
+            ).show()
+            alertDialog.dismiss()
+        }
+
+        buttonBack.setOnClickListener {
+            alertDialog.dismiss()
+        }
     }
 }
