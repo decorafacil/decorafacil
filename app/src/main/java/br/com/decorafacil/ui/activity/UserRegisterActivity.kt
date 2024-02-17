0ce36ca4459dea1a3401a054da0f8be4eceb0f90
Thyago Lobato <thyagollobato@gmail.com>
2024-02-17 11:07:13 -0300

    refact: atualiza classe 'UserRegisterActivity' para utilizar o View Binding

--- app/src/main/java/br/com/decorafacil/ui/activity/UserRegisterActivity.kt
+++ app/src/main/java/br/com/decorafacil/ui/activity/UserRegisterActivity.kt
@@ -1,26 +1,26 @@
 package br.com.decorafacil.ui.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.Toast
-import br.com.decorafacil.R
+import androidx.appcompat.app.AppCompatActivity
+import br.com.decorafacil.databinding.ActivityUserRegisterBinding
 import br.com.decorafacil.infra.inmemory.UserRepositoryInMemory
 import br.com.decorafacil.models.Address
 import br.com.decorafacil.models.User
 import br.com.decorafacil.repository.UserRepository
-import java.lang.Exception
 
 class UserRegisterActivity : AppCompatActivity() {
 
     private val userRepository: UserRepository = UserRepositoryInMemory()
 
+    private val binding by lazy {
+        ActivityUserRegisterBinding.inflate(layoutInflater);
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_register)
+        setContentView(binding.root)
         configComponents()
     }
 
@@ -30,7 +30,7 @@ class UserRegisterActivity : AppCompatActivity() {
     }
 
     private fun configBackArrowButton() {
-        val buttonBackArrow = findViewById<ImageView>(R.id.imageViewUserLeftArrow)
+        val buttonBackArrow = binding.imageViewUserLeftArrow
         buttonBackArrow.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
@@ -38,21 +38,22 @@ class UserRegisterActivity : AppCompatActivity() {
     }
 
     private fun saveUserData() {
-        val buttonSaveUserInformation = findViewById<ImageButton>(R.id.imageButtonSaveUser)
+        val buttonSaveUserInformation = binding.imageButtonSaveUser
         buttonSaveUserInformation.setOnClickListener {
-            val fantasyName = findViewById<EditText>(R.id.editTextFantasyName).text.toString()
-            val name = findViewById<EditText>(R.id.editTextUserName).text.toString()
-            val cpf = findViewById<EditText>(R.id.editTextUserCpf).text.toString()
-            val phoneNumber = findViewById<EditText>(R.id.editTextUserPhoneNumber).text.toString()
-            val email = findViewById<EditText>(R.id.editTextUserEmail).text.toString()
-            val street = findViewById<EditText>(R.id.editTextUserStreetAddress).text.toString()
-            val addressNumber = findViewById<EditText>(R.id.editTextUserAddressNumber).text.toString()
-            val district = findViewById<EditText>(R.id.editTextUserDistrict).text.toString()
-            val complement = findViewById<EditText>(R.id.editTextUserComplement).text.toString()
-            val city = findViewById<EditText>(R.id.editTextUserCity).text.toString()
-            val address = Address(street, district, city, "", addressNumber, "", complement)
-            val password = findViewById<EditText>(R.id.editTextUserPassword).text.toString()
-            val confirmPassword = findViewById<EditText>(R.id.editTextUserConfirmPassword).text.toString()
+            val fantasyName = binding.editTextFantasyName.text.toString()
+            val name = binding.editTextUserName.text.toString()
+            val cpf = binding.editTextUserCpf.text.toString()
+            val phoneNumber = binding.editTextUserPhoneNumber.text.toString()
+            val email = binding.editTextUserEmail.text.toString()
+            val zipCode = binding.editTextUserZipCode.text.toString()
+            val street = binding.editTextUserStreetAddress.text.toString()
+            val addressNumber = binding.editTextUserAddressNumber.text.toString()
+            val district = binding.editTextUserDistrict.text.toString()
+            val complement = binding.editTextUserComplement.text.toString()
+            val city = binding.editTextUserCity.text.toString()
+            val address = Address(street, district, city, "", addressNumber, zipCode, complement)
+            val password = binding.editTextUserPassword.text.toString()
+            val confirmPassword = binding.editTextUserConfirmPassword.text.toString()
             if (password == confirmPassword) {
                 val user = User(
                     fantasyName,
