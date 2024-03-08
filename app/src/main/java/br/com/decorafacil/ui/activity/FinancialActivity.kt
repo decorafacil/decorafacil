7039fe886a281afe4cc77691d096cb761d52a770
Thyago Lobato <thyagollobato@gmail.com>
2024-03-08 18:21:59 -0300

    feat: cria primeira versão da tela de relatório financeiro

---
+++ app/src/main/java/br/com/decorafacil/ui/activity/FinancialActivity.kt
@@ -0,0 +1,12 @@
+package br.com.decorafacil.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.decorafacil.R
+
+class FinancialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_financial)
+    }
+}
